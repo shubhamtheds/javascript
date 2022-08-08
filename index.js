603d571a1c6b699dab554a281939945b2baf012d
Shubham Kumar Sharma <89303017+shubhamtheds@users.noreply.github.com>
2022-08-08 16:58:03 +0000

    for loop

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const shubham = ['shubham'];
 
 function findShubham1(array) {
-  for (let i=0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (array[i] === 'shubham') {
       console.log('Found SHUBHAM!');
     }
@@ -12,11 +12,11 @@ function findShubham1(array) {
 
 findShubham1(shubham);
 
-//#1 -- For loop in Javascript.
-// const fish = ['dory', 'bruce', 'marlin', 'nemo'];
-// const nemo = ['nemo'];
-// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-// const large = new Array(10000).fill('nemo');
+//1 -- For loop in Javascript.
+const fish = ['dory', 'bruce', 'marlin', 'nemo'];
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(10000).fill('nemo');
 
 // function findNemo2(fish) {
 //   let t0 = performance.now();
@@ -148,3 +148,102 @@ findShubham1(shubham);
 // }
 
 // arrayOfHiNTimes(6)
+
+
+
+
+
+
+
+// Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items
+//For Example:
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'i'];
+//should return false.
+//-----------
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
+//should return true.
+
+// 2 parameters - arrays - no size limit
+// return true or false
+
+// function containsCommonItem(arr1, arr2) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//       if (arr1[i] === arr2[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false
+// }
+
+// //O(a*b)
+// //O(1) - Space Complexity
+
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'a'];
+
+// function containsCommonItem2(arr1, arr2) {
+//   // loop through first array and create object where properties === items in the array
+//   // can we assume always 2 params?
+
+//   let map = {};
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (!map[arr1[i]]) {
+//       const item = arr1[i];
+//       map[item] = true;
+//     }
+//   }
+//   // loop through second array and check if item in second array exists on created object. 
+//   for (let j = 0; j < arr2.length; j++) {
+//     if (map[arr2[j]]) {
+//       return true;
+//     }
+//   }
+//   return false
+// }
+
+// //O(a + b) Time Complexity
+// //O(a) Space Complexity
+
+// // containsCommonItem2(array1, array2)
+
+// function containsCommonItem3(arr1, arr2) {
+//   return arr1.some(item => arr2.includes(item))
+// }
+
+// containsCommonItem(array1, array2)
+// containsCommonItem2(array1, array2)
+// containsCommonItem3(array1, array2)
+
+
+
+
+
+// // Naive
+// function hasPairWithSum(arr, sum){
+//   var len = arr.length;
+//   for(var i =0; i<len-1; i++){
+//      for(var j = i+1;j<len; j++){
+//         if (arr[i] + arr[j] === sum)
+//             return true;
+//      }
+//   }
+
+//   return false;
+// }
+
+// // Better
+// function hasPairWithSum2(arr, sum){
+//   const mySet = new Set();
+//   const len = arr.length;
+//   for (let i = 0; i < len; i++){
+//     if (mySet.has(arr[i])) {
+//       return true;
+//     }
+//     mySet.add(sum - arr[i]);
+//   }
+//   return false;
+// }
+
+// hasPairWithSum2([6,4,3,2,1,7], 9)
