fdbc93f81b44dc1307a8176eb9e794ab2f313b79
Shubham Kumar Sharma <89303017+shubhamtheds@users.noreply.github.com>
2022-08-25 15:54:23 +0000

    Added Graphs class

--- index.js
+++ index.js
@@ -1,90 +1,46 @@
-class DoublyLinkedList {
-  constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-      prev: null
-    };
-    this.tail = this.head;
-    this.length = 1;
-  }
-  append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-      prev: null
-    }
-    console.log(newNode)
-    newNode.prev = this.tail
-    this.tail.next = newNode;
-    this.tail = newNode;
-    this.length++;
-    return this;
-  }
-  prepend(value) {
-    const newNode = {
-      value: value,
-      next: null,
-      prev: null
-    }
-    newNode.next = this.head;
-    this.head.prev = newNode
-    this.head = newNode;
-    this.length++;
-    return this;
-  }
-  printList() {
-    const array = [];
-    let currentNode = this.head;
-    while (currentNode !== null) {
-      array.push(currentNode.value)
-      currentNode = currentNode.next
-    }
-    return array;
-  }
-  insert(index, value) {
-    //Check for proper parameters;
-    if (index >= this.length) {
-      return this.append(value);
-    }
+class Graph { 
+  constructor() { 
+    this.numberOfNodes = 0; 
+    this.adjacentList = {}; 
+  } 
+  addVertex(node)  { 
+    this.adjacentList[node] = []; 
+    this.numberOfNodes++;
+  } 
+  addEdge(node1, node2) { 
+    //uniderected Graph 
+    this.adjacentList[node1].push(node2); 
+    this.adjacentList[node2].push(node1); 
+  } 
+  showConnections() { 
+    const allNodes = Object.keys(this.adjacentList); 
+    for (let node of allNodes) { 
+      let nodeConnections = this.adjacentList[node]; 
+      let connections = ""; 
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      } 
+      console.log(node + "-->" + connections); 
+    } 
+} 
+} 
 
-    const newNode = {
-      value: value,
-      next: null,
-      prev: null
-    }
-    const leader = this.traverseToIndex(index - 1);
-    const follower = leader.next;
-    leader.next = newNode;
-    newNode.prev = leader;
-    newNode.next = follower;
-    follower.prev = newNode;
-    this.length++;
-    console.log(this)
-    return this.printList();
-  }
-  traverseToIndex(index) {
-    //Check parameters
-    let counter = 0;
-    let currentNode = this.head;
-    while (counter !== index) {
-      currentNode = currentNode.next;
-      counter++;
-    }
-    return currentNode;
-  }
-}
+var myGraph = new Graph();
+myGraph.addVertex('0');
+myGraph.addVertex('1');
+myGraph.addVertex('2');
+myGraph.addVertex('3');
+myGraph.addVertex('4');
+myGraph.addVertex('5');
+myGraph.addVertex('6');
+myGraph.addEdge('3', '1'); 
+myGraph.addEdge('3', '4'); 
+myGraph.addEdge('4', '2'); 
+myGraph.addEdge('4', '5'); 
+myGraph.addEdge('1', '2'); 
+myGraph.addEdge('1', '0'); 
+myGraph.addEdge('0', '2'); 
+myGraph.addEdge('6', '5');
 
-let myLinkedList = new DoublyLinkedList(10);
-myLinkedList.append(5)
-myLinkedList.append(16)
-myLinkedList.prepend(1)
-myLinkedList.insert(2, 99)
-console.log(myLinkedList.printList())
-// myLinkedList.insert(20, 88)
-// myLinkedList.printList()
-// myLinkedList.remove(2)
-// myLinkedList.reverse()
-
-
-
+myGraph.showConnections(); 
